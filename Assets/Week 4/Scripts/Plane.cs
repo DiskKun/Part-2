c86002fff1cfa298832a990a71737a35348d5a49
Lucas Hourahine <luca5h1905@gmail.com>
2024-02-01 10:26:12 -0500

    Week 4 Task 2 Commit 1

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -8,14 +8,49 @@ public class Plane : MonoBehaviour
     public float newPositionThreshold = 0.2f;
     Vector2 lastPosition;
     LineRenderer lineRenderer;
+    Vector2 currentPosition;
+    Rigidbody2D rb;
+    public float speed = 1;
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        rb = GetComponent<Rigidbody2D>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
+
     }
 
+    private void FixedUpdate()
+    {
+        currentPosition = transform.position;
+        if (points.Count > 0)
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rb.rotation = -angle;
+        }
+        rb.MovePosition(rb.position + (Vector2)transform.up * speed * Time.deltaTime);
+        
+    }
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, currentPosition);
+
+        if (points.Count > 0)
+        {
+            if (Vector2.Distance(currentPosition, points[0]) < newPositionThreshold)
+            {
+                points.RemoveAt(0);
+
+                for (int i = 0; i < lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                }
+                lineRenderer.positionCount--;
+            }
+        }
+    }
 
     private void OnMouseDown()
     {
@@ -23,6 +58,9 @@ public class Plane : MonoBehaviour
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
     }
+
+
+
     private void OnMouseDrag()
     {
         Vector2 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
